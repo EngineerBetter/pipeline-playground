a2938e73298301b195f0f3d461f2288338f4f9ac
Sam Colson <sam.colson@engineerbetter.com>
2021-11-12 11:46:17 +0000

    Change state

--- tf/terraform.tf
+++ tf/terraform.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "playground-bucket-bao"
-    prefix = "terraform/state"
+    # bucket = "playground-bucket-bao"
   }
 }
 
@@ -9,8 +8,3 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
-
-# data "google_storage_bucket" "bao-bucket" {
-#   name = "playground-bucket-bao"
-#   # location = "eu-west2"
-#   }
