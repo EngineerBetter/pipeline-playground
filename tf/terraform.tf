aa9a5f993612d742f14ad90d1bccdc3196b70282
Sam Colson <sam.colson@engineerbetter.com>
2021-11-12 11:38:08 +0000

    Update bucket

--- tf/terraform.tf
+++ tf/terraform.tf
@@ -6,3 +6,8 @@ provider "google" {
   project = var.project_id
   region  = var.region
 }
+
+resource "google_storage_bucket" "bao-bucket" {
+  name = "playground-bucket-bao"
+  location = "eu-west2"
+  }
