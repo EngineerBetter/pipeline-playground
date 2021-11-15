56118e5bc8ccbc1fa80ae7eb2347c182f6f125dc
Richard Pryce <rich_pryce@yahoo.co.uk>
2021-11-15 11:34:31 +0000

    Create gcp bucket

--- tf/terraform.tf
+++ tf/terraform.tf
@@ -1,7 +1,12 @@
 terraform {
+
 }
 
 provider "google" {
   project = var.project_id
   region  = var.region
+}
+
+resource "google_storage_bucket" "my-lovely-bucket" {
+  name = "my-loveley-bucket"
 }
